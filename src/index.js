8aeb56754e5d02c1526098bc229134fbb88939ee
itsayush30 <itsayush30@gmail.com>
2023-12-25 16:24:09 +0530

    Made proxy complient with rewrite path from gateway

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
+// app.use('/flightsService/api', apiRoutes);
+
 
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
